253a7c906b4ea5f06c8917ce436e21cf6bff7f00
dan <dan.pupaza@atos.net>
2019-12-30 20:59:44 +0200

    Initial commit

---
+++ src/main/java/ro/appvalue/quarkus/HelloRestController.java
@@ -0,0 +1,27 @@
+package ro.appvalue.quarkus;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public class HelloRestController {
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        String hostname = null;
+        try {
+            hostname = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            hostname = "unknown";
+        }
+        
+        return "Quarkus, hello from " + hostname;
+    }
+}
